dff0f4b09a1d9478e2dccc895a2a0b5744d2c8b5
vennela <vennelacheekoti@gmail.com>
2020-07-05 19:51:59 -0400

    resolving deployment issues

--- server/server.js
+++ server/server.js
@@ -55,6 +55,6 @@ app.delete('/users/:id', (req, res) => {
 	})
   })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log(`Example app listening on port 3000!`)
 })
